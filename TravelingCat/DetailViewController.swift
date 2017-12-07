70362fd1fd805e161ff50f2c1de54b41b81ae1f4
Mo0nBunny <2tigra@gmail.com>
2017-12-07 23:52:26 +0800

    taskarry was changed to taskdata class

--- TravelingCat/DetailViewController.swift
+++ TravelingCat/DetailViewController.swift
@@ -14,18 +14,21 @@ class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDa
     @IBOutlet weak var backgroundImage: UIImageView!
     
     var category: CaterogyData?
-    var taskArray = [String]()
+    var taskArray = [TaskData]()
     
       
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        taskArray.append(TaskData(task: ""))
+
         taskTableView.backgroundView = UIImageView(image: UIImage(named: "background"))
         
         if let detailCategory = self.category {
             navigationItem.title = detailCategory.title
         }
-        for taskItem in taskArray {
+       
+        for taskItem: TaskData in ToDoTask.whatToDo {
             taskArray.append(taskItem)
         }
     }
@@ -39,22 +42,23 @@ class DetailViewController: UIViewController, UITableViewDelegate, UITableViewDa
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return taskArray.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! DetailTableViewCell
         cell.inputTask.delegate = self
-        cell.inputTask.text = ""
-        cell.inputTask.selectAll(nil)
+//        self.taskArray.append(cell.inputTask.text!)
+        cell.inputTask.text = taskArray[indexPath.row].task
         cell.backgroundColor = UIColor.clear
         return cell
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        taskArray.append(textField.text!)
-//        taskTableView.reloadData()
-        print(taskArray)
+        self.taskArray.append(TaskData(task: textField.text!))
+//        self.taskArray.append(TaskData(task:""))
+        self.taskTableView.reloadData()
+        print(self.taskArray)
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
---
+++ TravelingCat/TodoTask.swift
@@ -0,0 +1,14 @@
+//
+//  todoTask.swift
+//  TravelingCat
+//
+//  Created by Sirin K on 07/12/2017.
+//  Copyright © 2017 Sirin K. All rights reserved.
+//
+
+import UIKit
+
+struct ToDoTask {
+    static var whatToDo : [TaskData] = []
+}
+
