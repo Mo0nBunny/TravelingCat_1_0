1c54b633fe3c7947aef1e560f2f13c71a04520c5
Mo0nBunny <2tigra@gmail.com>
2018-03-26 13:54:53 +0800

    - created trip tableView controller

--- TravelingCat/AppDelegate.swift
+++ TravelingCat/AppDelegate.swift
@@ -25,10 +25,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
                                                                 NSAttributedStringKey.font: barFont]
         }
         //MARK: - To start from root view
-        let splitViewController = self.window!.rootViewController as! UISplitViewController
-        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
-        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
-        splitViewController.delegate = self
+//        let splitViewController = self.window!.rootViewController as! UISplitViewController
+//        let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count-1] as! UINavigationController
+//        navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
+//        splitViewController.delegate = self
         return true
     }
     
